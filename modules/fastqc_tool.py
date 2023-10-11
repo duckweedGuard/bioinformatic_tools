36c8d44a1c1aaee8f284dd0ee3301160b1f2b41f
Victoria Orlova <vikaorl1003@mail.ru>
2023-10-11 23:14:31 +0300

    Add read_fastq and write_output_fastq functions

--- modules/fastqc_tool.py
+++ modules/fastqc_tool.py
@@ -50,7 +50,52 @@ def count_quality(seq: tuple, quality_threshold: int = 0) -> bool:
     for sign in seq[1]:
         quality_result += (ord(sign) - 33)
     quality_out = quality_result / len(seq[1])
-    if quality_out > quality_threshold:
+    if quality_out < quality_threshold:
         return False
     else:
         return True
+
+
+def read_fastq(input_path: str, input_filename: str) -> dict:
+    """
+    Function to read a file by a given path and file name. Then the data is translated into a dictionary
+
+    :param input_path: given path where input file is
+    :param input_filename: input file name
+    :return: dictionary consisting of fastq sequences
+    """
+    if not os.path.isfile(os.path.join(input_path, input_filename)):
+        raise ValueError('No such fastq file in directory')
+    with open(os.path.join(input_path, 'fastq.fastq'), mode='r') as file:
+        fastq_dict = {}
+        for line in file:
+            if ' ' in line and '+' not in line:
+                seq_line = file.readline().strip()
+                file.readline()
+                quality_line = file.readline().strip()
+                fastq_dict[line] = tuple([seq_line, quality_line])
+        return fastq_dict
+
+
+def write_output_fastq(out_dict: dict, input_filename: str,
+                       output_data_dir: str = 'fastq_filtrator_resuls',
+                       output_filename: str = None):
+    """
+    Function for writing filtered fastq sequences
+
+    :param out_dict: post-work filtered dictionary
+    :param input_filename: input file name
+    :param output_data_dir: path where the file with filtered sequences will be saved
+    :param output_filename: name file with filtered sequences will be saved
+    """
+    if not os.path.isdir(output_data_dir):
+        os.mkdir(output_data_dir)
+    if output_filename is None:
+        output_filename = 'filtered_' + input_filename
+    with open(os.path.join(output_data_dir, output_filename), mode='w') as file:
+        for key in out_dict:
+            file.write(key)
+            file.write(out_dict[key][0] + "\n")
+            file.write("+" + key)
+            file.write(out_dict[key][1] + "\n")
+
